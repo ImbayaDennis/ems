348c14b80f646cd267e62249151f74f7007c414c
ImbayaDennis <dimbaya17@alustudent.com>
2023-02-08 12:45:23 +0300

    Update MetricsCard.tsx

--- src/components/common/MetricsCard.tsx
+++ src/components/common/MetricsCard.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 
 type Props = {
@@ -18,19 +19,27 @@ const MetricsCard = ({
   metric_three_label = "Metric 3",
 }: Props) => {
   return (
-    <div className="h-3/5 md:h-2/5 my-4 flex w-full min-w-[24rem] min-h-max max-w-2xl flex-col items-center justify-center rounded-lg bg-slate-300 p-4 shadow-md  dark:bg-slate-600">
-      <div className="flex h-1/2 w-36 flex-col items-center justify-center">
-        <p className="h-20 w-full text-center text-3xl">{metric_one}</p>
-        <p className="h-14 w-full text-center text-base">{metric_one_label}</p>
-      </div>
+    <div className="my-4 flex h-3/5 min-h-max w-full min-w-[24rem] max-w-2xl flex-col items-center justify-center rounded-lg bg-slate-300 p-4 shadow-md dark:bg-slate-600  md:h-2/5">
+      <Link href="/employee-manager">
+        <div className="flex h-1/2 w-36 flex-col items-center justify-center">
+          <p className="h-20 w-full text-center text-3xl">{metric_one}</p>
+          <p className="h-14 w-full text-center text-base">
+            {metric_one_label}
+          </p>
+        </div>
+      </Link>
       <div className="flex h-1/2 w-full flex-col justify-around p-4  sm:flex-row">
         <div className="w-46 flex h-20 flex-col items-center justify-center p-4">
           <p className="h-12 w-full text-center text-2xl">{metric_two}</p>
-          <p className="h-14 w-full text-center text-base">{metric_two_label}</p>
+          <p className="h-14 w-full text-center text-base">
+            {metric_two_label}
+          </p>
         </div>
         <div className="w-46 flex h-20 flex-col items-center justify-center p-4">
           <p className="h-12 w-full text-center text-2xl">{metric_three}</p>
-          <p className="h-14 w-full text-center text-base">{metric_three_label}</p>
+          <p className="h-14 w-full text-center text-base">
+            {metric_three_label}
+          </p>
         </div>
       </div>
     </div>
